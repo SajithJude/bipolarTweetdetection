89b1534afa5960e14cc2f49bea50bfd596fa714e
Jude Sajith <65233121+SajithJude@users.noreply.github.com>
2023-02-26 04:10:44 +0000

    shsh

--- app.py
+++ app.py
@@ -95,7 +95,7 @@ selector = alt.selection_single(name="SelectorName", fields=['cutoff'],
 input_dropdown = alt.binding_select(options=['False','True',], name='bp_label')
 selection = alt.selection_single(fields=['bp_label'], bind=input_dropdown)
 colaor = alt.condition(selection,
-                    alt.Color('bp_label:N', legend=None),
+                    alt.Color('bp_label:False', legend=None),
                     alt.value('green'))
 
 
