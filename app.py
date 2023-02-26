f01ab9b53ea386f23549f8a14bca77853d0fdc27
Jude Sajith <65233121+SajithJude@users.noreply.github.com>
2023-02-26 01:33:49 +0000

    dndn

--- app.py
+++ app.py
@@ -20,13 +20,19 @@ def plot_timeseries(df):
 data = pd.read_csv('user_1.csv', index_col=1)
 # data['date'] = pd.to_datetime(data['timestamp'])
 
+sentiments = []
+for text in data['tweet']:
+    sentiment = get_sentiment(text)
+    sentiments.append(sentiment)
+
+# Add a new column 'sentiment' to the DataFrame with the calculated sentiment scores
+data['sentiment'] = sentiments
+
 st.dataframe(data, width=800, height=500)
-# Calculate sentiment score for each row
-# dt = pd.DataFrame()
-# dt= data['tweet'].apply(get_sentiment)
+# data['sentiment'] = data['tweet'].apply(get_sentiment)
 
 # Group the data by date and calculate the mean sentiment score
 # sentiment_by_date = data.groupby('date')['bp_label'].mean()
 
 # Plot the timeseries graph
-plot_timeseries(data)
+#plot_timeseries(data)
