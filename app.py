741c81a38e6d4375c92ec9c39e658d07ee90c798
Jude Sajith <65233121+SajithJude@users.noreply.github.com>
2023-02-26 02:48:37 +0000

    jjj

--- app.py
+++ app.py
@@ -111,7 +111,7 @@ annotations_df["y"] = 0
 # )
 
 c = alt.Chart(annotations_df).mark_circle().encode(
-    x='timestamp', y='tweet', size='sentiment', color='c', tooltip=['timestamp', 'tweet', 'sentiment'])
+    x='timestamp', y='tweet', tooltip=['timestamp', 'tweet'])
 
 # Display both charts together
 st.altair_chart((c).interactive(), theme="streamlit",use_container_width=False)
