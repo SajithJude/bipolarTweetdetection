730264d32244e6ab53529feb7181b8f115d1fdef
Jude Sajith <65233121+SajithJude@users.noreply.github.com>
2023-02-26 01:14:33 +0000

    ss

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ def get_sentiment(text):
 # Define a function to plot the timeseries graph
 def plot_timeseries(df):
     fig, ax = plt.subplots()
-    ax.plot(df.index, df[['bp_label']])
+    ax.plot(df.index, df['bp_label'])
     ax.set_title('Sentiment Analysis over Time')
     ax.set_xlabel('Date')
     ax.set_ylabel('Sentiment Score')
@@ -18,16 +18,15 @@ def plot_timeseries(df):
 
 # Load the data
 data = pd.read_csv('user_1.csv', index_col=1)
-# data['date'] = pd.to_datetime(data['timestamp'])
+data['date'] = pd.to_datetime(data['timestamp'])
 
 st.dataframe(data, width=800, height=500)
-
 # Calculate sentiment score for each row
 # dt = pd.DataFrame()
 # dt= data['tweet'].apply(get_sentiment)
 
 # Group the data by date and calculate the mean sentiment score
-sentiment_by_date = data.groupby('timestamp')['bp_label'].mean()
+sentiment_by_date = data.groupby('date')['bp_label'].mean()
 
 # Plot the timeseries graph
 plot_timeseries(sentiment_by_date)
