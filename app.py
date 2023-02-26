4bb7b4d814cdd919b92b59511d4d49a77c70a5e2
Jude Sajith <65233121+SajithJude@users.noreply.github.com>
2023-02-25 19:30:30 -0500

    Create app.py

---
+++ app.py
@@ -0,0 +1,30 @@
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+from textblob import TextBlob
+
+# Define a function to get sentiment analysis
+def get_sentiment(text):
+    return TextBlob(text).sentiment.polarity
+
+# Define a function to plot the timeseries graph
+def plot_timeseries(df):
+    fig, ax = plt.subplots()
+    ax.plot(df.index, df['sentiment'])
+    ax.set_title('Sentiment Analysis over Time')
+    ax.set_xlabel('Date')
+    ax.set_ylabel('Sentiment Score')
+    st.pyplot(fig)
+
+# Load the data
+data = pd.read_csv('data.csv', index_col=0)
+data['date'] = pd.to_datetime(data['date'])
+
+# Calculate sentiment score for each row
+data['sentiment'] = data['text'].apply(get_sentiment)
+
+# Group the data by date and calculate the mean sentiment score
+sentiment_by_date = data.groupby('date')['sentiment'].mean()
+
+# Plot the timeseries graph
+plot_timeseries(sentiment_by_date)
