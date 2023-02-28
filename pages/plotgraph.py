14718753a3620dc5bd4b3f471cdf25808185f0b7
Jude Sajith <65233121+SajithJude@users.noreply.github.com>
2023-02-28 23:33:22 +0000

    hshsh

--- pages/plotgraph.py
+++ pages/plotgraph.py
@@ -7,27 +7,27 @@ uf = st.file_uploader("Choose a CSV file", type=["csv"])
 
 # If a file was uploaded, read the contents into a Pandas DataFrame
 if uf is not None:
-    df = pd.read_csv(uf)
-    df['timestamp'] = pd.to_datetime(df['timestamp'])
-    st.table(df.head(10))
+    df2 = pd.read_csv(uf)
+    df2['timestamp'] = pd.to_datetime(df2['timestamp'])
+    st.table(df2.head(10))
         # Convert 'timestamp' column to a pa
-    # Load data from CSV file
-    # df = pd.read_csv('data.csv')
+    # Load dta from CSV file
+    # df2 = pd.read_csv('data.csv')
 
     # Create a checkbox to toggle data points with true/false labels
     show_true = st.checkbox('Show True Labels')
 
     # Filter the data based on the checkbox value
     if show_true:
-        data = df
+        dta = df2
     else:
-        data = df[df['bp_label'] == False]
+        dta = df2[df2['bp_label'] == False]
 
     # Create a selection for the tooltip
     selection = alt.selection_single(fields=['timestamp'], nearest=True, on='mouseover', empty='none')
 
     # Create the chart
-    chart = alt.Chart(data).mark_circle().encode(
+    chart = alt.Chart(dta).mark_circle().encode(
         x='timestamp:T',
         y='sentiment:Q',
         color=alt.Color('bp_label:N', scale=alt.Scale(domain=['TRUE', 'FALSE'], range=['red', 'blue'])),
