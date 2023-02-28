295c533aac63c8cdd85c4beac65cc77357b44c8c
Jude Sajith <65233121+SajithJude@users.noreply.github.com>
2023-02-28 20:23:52 +0000

    nssn

--- pages/1_Analytics.py
+++ pages/1_Analytics.py
@@ -1,71 +1,30 @@
 import streamlit as st
-import pandas as pd
-# import altair as alt
+import numpy as np
+from transformers import BertTokenizer, BertForSequenceClassification
+import torch
+
+@st.cache(allow_output_mutation=True)
+def get_model():
+    tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+    model = BertForSequenceClassification.from_pretrained("Joom/bert-base-uncased-NisadiBipolar")
+    return tokenizer,model
 
 
-# alt.themes.enable("streamlit")
-# # Define a function to get sentiment analysis
-# def get_sentiment(text):
-#     return TextBlob(text).sentiment.polarity
+tokenizer,model = get_model()
 
+user_input = st.text_area('Enter what the patient tells to Analyze depression')
+button = st.button("Analyze")
 
-# def get_keyword(text):
-#     return TextBlob(text).noun_phrases
+d = {
+    
+  1:'No signs of Bipolar Disorder',
+  0:'Possible Signs of Bipolar Disorder'
+}
 
-# global source
-# annotations_df = pd.DataFrame(source, columns=["timestamp", "sentiment","tweet","bp_label"])
-# annotations_df.timestamp = pd.to_datetime(annotations_df.timestamp)
-# annotations_df["y"] = 0
-
-# slider = alt.binding_range(min=-1, max=1, step=0.01, name='SentimentFilter:')
-# selector = alt.selection_single(name="SelectorName", fields=['cutoff'],
-#                                 bind=slider, init={'cutoff': 0.5})
-
-# c = alt.Chart(annotations_df).mark_point().encode(
-#     x='timestamp', y='sentiment',
-#     tooltip=['tweet','bp_label'] ,color=alt.condition(
-#         alt.datum.sentiment < selector.cutoff,
-#         alt.value('red'), alt.value('blue'))).add_selection( selector )
-# st.altair_chart((c).interactive(), theme="streamlit",use_container_width=True)
-
-
-
-# def get_chart(data):
-#     hover = alt.selection_single(
-#         fields=["timestamp"],
-#         nearest=True,
-#         on="mouseover",
-#         empty="none",
-#     )
-
-#     lines = (
-#         alt.Chart(data, height=500, title="Tweet Analysis of User")
-#         .mark_line()
-#         .encode(
-#             x=alt.X("timestamp", title="Date"),
-#             y=alt.Y("sentiment", title="Price"),
-#             color="symbol",
-#         )
-#     )
-
-#     # Draw points on the line, and highlight based on selection
-#     points = lines.transform_filter(hover).mark_circle(size=65)
-
-#     # Draw a rule at the location of the selection
-#     tooltips = (
-#         alt.Chart(data)
-#         .mark_rule()
-#         .encode(
-#             x="timestamp",
-#             y="sentiment",
-#             opacity=alt.condition(hover, alt.value(0.3), alt.value(0)),
-#             tooltip=[
-#                 alt.Tooltip("timestamp", title="Date"),
-#                 alt.Tooltip("sentiment", title="Price (USD)"),
-#             ],
-#         )
-#         .add_selection(hover)
-#     )
-
-#     return (lines + points + tooltips).interactive()
-
+if user_input and button :
+    test_sample = tokenizer([user_input], padding=True, truncation=True, max_length=256,return_tensors='pt')
+    # test_sample
+    output = model(**test_sample)
+    st.write("Logits: ",output.logits)
+    y_pred = np.argmax(output.logits.detach().numpy(),axis=1)
+    st.write("Prediction: ",d[y_pred[0]])
