efbb70232d2bcd20ebe93f9805766b9b603bb744
Jude Sajith <65233121+SajithJude@users.noreply.github.com>
2023-02-28 23:34:04 +0000

    sjjs

--- app.py
+++ app.py
@@ -68,22 +68,22 @@ if selected_file:
     # st.sidebar.write("Final Year Undergraduate")
     # st.sidebar.write("IIT (University of Westminster)")
 
-with st.beta_expander("Click to expand Graph"):
+# with st.beta_expander("Click to expand Graph"):
 
-    annotations_df = pd.DataFrame(source, columns=["timestamp", "sentiment","tweet","bp_label"])
-    annotations_df.timestamp = pd.to_datetime(annotations_df.timestamp)
-    annotations_df["y"] = 0
+#     annotations_df = pd.DataFrame(source, columns=["timestamp", "sentiment","tweet","bp_label"])
+#     annotations_df.timestamp = pd.to_datetime(annotations_df.timestamp)
+#     annotations_df["y"] = 0
 
-    slider = alt.binding_range(min=-1, max=1, step=0.01, name='SentimentFilter:')
-    selector = alt.selection_single(name="SelectorName", fields=['cutoff'],
-                                    bind=slider, init={'cutoff': 0.5})
+#     slider = alt.binding_range(min=-1, max=1, step=0.01, name='SentimentFilter:')
+#     selector = alt.selection_single(name="SelectorName", fields=['cutoff'],
+#                                     bind=slider, init={'cutoff': 0.5})
 
-    c = alt.Chart(annotations_df).mark_point().encode(
-        x='timestamp', y='sentiment',
-        tooltip=['tweet','bp_label'] ,color=alt.condition(
-            alt.datum.sentiment < selector.cutoff,
-            alt.value('red'), alt.value('blue'))).add_selection( selector )
-    st.altair_chart((c).interactive(), theme="streamlit",use_container_width=True)
+#     c = alt.Chart(annotations_df).mark_point().encode(
+#         x='timestamp', y='sentiment',
+#         tooltip=['tweet','bp_label'] ,color=alt.condition(
+#             alt.datum.sentiment < selector.cutoff,
+#             alt.value('red'), alt.value('blue'))).add_selection( selector )
+#     st.altair_chart((c).interactive(), theme="streamlit",use_container_width=True)
 
 
     # st.write("Content inside the expandable section")
