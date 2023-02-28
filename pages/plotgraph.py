32f48f92cc1d956cf100160a5385b0a67b5b67b1
Jude Sajith <65233121+SajithJude@users.noreply.github.com>
2023-02-28 23:09:54 +0000

    jxjxj

--- pages/plotgraph.py
+++ pages/plotgraph.py
@@ -27,9 +27,9 @@ if uploaded_file is not None:
     # Create the chart
     chart = alt.Chart(data).mark_circle().encode(
         x='timestamp:T',
-        y='sentiment_score:Q',
-        color=alt.Color('label:N', scale=alt.Scale(domain=['True', 'False'], range=['red', 'blue'])),
-        tooltip=['description:N', 'keywords:N']
+        y='sentiment:Q',
+        color=alt.Color('bp_label:N', scale=alt.Scale(domain=['True', 'False'], range=['red', 'blue'])),
+        tooltip=['description:N']
     ).add_selection(selection)
 
     # Add a line that follows the selected timestamp
