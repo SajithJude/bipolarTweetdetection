a7e26474ed21ffb7739788142963826bf7051edc
Jude Sajith <65233121+SajithJude@users.noreply.github.com>
2023-03-01 00:03:35 +0000

    bbb

--- pages/plotgraph.py
+++ pages/plotgraph.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import altair as alt
-
+alt.themes.enable("streamlit")
 
 uf = st.file_uploader("Choose a CSV file", type=["csv"])
 
@@ -15,29 +15,29 @@ if uf is not None:
     # df2 = pd.read_csv('data.csv')
 
     # Create a checkbox to toggle data points with true/false labels
-show_true = st.checkbox('Show True Labels')
+    show_true = st.checkbox('Show True Labels')
 
-# Filter the data based on the checkbox value
-if show_true:
-    dta = df2
-else:
+    # Filter the data based on the checkbox value
+    if show_true:
+        dta = df2
+    else:
 
-    dta = df2[df2['bp_label'] == False]
+        dta = df2[df2['bp_label'] == False]
 
 
-# Create a selection for the tooltip
-selection = alt.selection_single(fields=['timestamp'], nearest=True, on='mouseover', empty='none')
+    # Create a selection for the tooltip
+    selection = alt.selection_single(fields=['timestamp'], nearest=True, on='mouseover', empty='none')
 
-# Create the chart
-chart1 = alt.Chart(dta).mark_circle(size=100).encode(
-    x='timestamp:T',
-    y='sentiment:Q',
-    color=alt.Color('bp_label:N', scale=alt.Scale(domain=['True', 'False'], range=['red', 'blue'])),
-    tooltip=['tweet:N']
-).add_selection(selection)
+    # Create the chart
+    chart1 = alt.Chart(dta).mark_circle().encode(
+        x='timestamp:T',
+        y='sentiment:Q',
+        color=alt.Color('bp_label:N', scale=alt.Scale(domain=['True', 'False'], range=['red', 'blue'])),
+        tooltip=['tweet:N']
+    ).add_selection(selection)
 
-# Add a line that follows the selected timestamp
-# line1 = chart1.transform_filter(selection).mark_line(color='black')
+    # Add a line that follows the selected timestamp
+    # line1 = chart1.transform_filter(selection).mark_line(color='black')
 
-# Add the chart and line to the Streamlit app
-st.altair_chart((chart1).interactive(), use_container_width=True)
+    # Add the chart and line to the Streamlit app
+    st.altair_chart((chart1).interactive(), use_container_width=True)
