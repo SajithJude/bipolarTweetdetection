be7b045bdfa6dcf77f597634016f6fd710f50ce7
Jude Sajith <65233121+SajithJude@users.noreply.github.com>
2023-03-01 06:19:53 +0000

    sss

--- pages/plotgraph.py
+++ pages/plotgraph.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 import mpld3
+import streamlit.components.v1 as components
 
 # Load the data
 data = pd.read_csv('data/streamlitdatabase.csv', parse_dates=['timestamp'])
@@ -14,15 +15,16 @@ patient_filter = st.slider('Patient index', 1, 25, 1)
 # Filter the data
 data_filtered = data[data['patient_index'] == patient_filter]
 
-if st.button('Plot'):
+# if st.button('Plot'):
 # Create the scatter plot
-    fig, ax = plt.subplots()
-    scatter = ax.scatter(data_filtered[x_var], data_filtered[y_var], c=data_filtered['bp_label'])
 
-    # Add tooltip with tweet text
-    tooltip = mpld3.plugins.PointHTMLTooltip(scatter, labels=data_filtered['tweet'])
-    mpld3.plugins.connect(fig, tooltip)
+fig, ax = plt.subplots()
+scatter = ax.scatter(data_filtered[x_var], data_filtered[y_var], c=data_filtered['bp_label'])
 
-    # Convert plot to HTML and display
-    html = mpld3.fig_to_html(fig)
-    st.write(html, unsafe_allow_html=True)
+# Add tooltip with tweet text
+tooltip = mpld3.plugins.PointHTMLTooltip(scatter, labels=data_filtered['tweet'])
+mpld3.plugins.connect(fig, tooltip)
+
+# Convert plot to HTML and display
+html = mpld3.fig_to_html(fig)
+components.html(html, unsafe_allow_html=True)
