215ea0d9f56c3d4be51fc89655a51c085ab3b78b
Jude Sajith <65233121+SajithJude@users.noreply.github.com>
2023-03-01 06:11:20 +0000

    snsn

--- pages/plotgraph.py
+++ pages/plotgraph.py
@@ -7,21 +7,22 @@ import mpld3
 data = pd.read_csv('data/streamlitdatabase.csv', parse_dates=['timestamp'])
 
 # Sidebar filters
-x_var = st.sidebar.selectbox('X-axis variable', ['hour', 'weekday', 'timestamp'])
-y_var = st.sidebar.selectbox('Y-axis variable', ['hour', 'weekday', 'timestamp'])
-patient_filter = st.sidebar.slider('Patient index', 1, 25, 1)
+x_var = st.selectbox('X-axis variable', ['hour', 'weekday', 'timestamp'])
+y_var = st.selectbox('Y-axis variable', ['hour', 'weekday', 'timestamp'])
+patient_filter = st.slider('Patient index', 1, 25, 1)
 
 # Filter the data
 data_filtered = data[data['patient_index'] == patient_filter]
 
+if st.button('Plot'):
 # Create the scatter plot
-fig, ax = plt.subplots()
-scatter = ax.scatter(data_filtered[x_var], data_filtered[y_var], c=data_filtered['bp_label'])
+    fig, ax = plt.subplots()
+    scatter = ax.scatter(data_filtered[x_var], data_filtered[y_var], c=data_filtered['bp_label'])
 
-# Add tooltip with tweet text
-tooltip = mpld3.plugins.PointHTMLTooltip(scatter, labels=data_filtered['tweet'])
-mpld3.plugins.connect(fig, tooltip)
+    # Add tooltip with tweet text
+    tooltip = mpld3.plugins.PointHTMLTooltip(scatter, labels=data_filtered['tweet'])
+    mpld3.plugins.connect(fig, tooltip)
 
-# Show the plot
-mpld3.show(fig)
+    # Show the plot
+    mpld3.show(fig)
 
