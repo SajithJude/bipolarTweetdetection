fc7bd5c281a9002b06b9d9df9b17862cd52d4a34
Jude Sajith <65233121+SajithJude@users.noreply.github.com>
2023-03-01 00:48:25 +0000

    hhh

--- pages/plotgraph.py
+++ pages/plotgraph.py
@@ -3,40 +3,36 @@ import pandas as pd
 import altair as alt
 alt.themes.enable("streamlit")
 
-uf = st.file_uploader("Choose a CSV file", type=["csv"])
+df2 = pd.read_csv("data/user_1.csv")
+df2['timestamp'] = pd.to_datetime(df2['timestamp'])
+st.table(df2.head(5))
+    # Convert 'timestamp' column to a pa
+# Load dta from CSV file
+# df2 = pd.read_csv('data.csv')
 
-# If a file was uploaded, read the contents into a Pandas DataFrame
-if uf is not None:
-    df2 = pd.read_csv(uf)
-    df2['timestamp'] = pd.to_datetime(df2['timestamp'])
-    st.table(df2.head(5))
-        # Convert 'timestamp' column to a pa
-    # Load dta from CSV file
-    # df2 = pd.read_csv('data.csv')
+# Create a checkbox to toggle data points with true/false labels
+show_true = st.checkbox('Show True Labels')
 
-    # Create a checkbox to toggle data points with true/false labels
-    show_true = st.checkbox('Show True Labels')
-
-    # Filter the data based on the checkbox value
-    if show_true:
-        dta = df2
-    else:
-        dta = df2[df2['bp_label'] == False]
+# Filter the data based on the checkbox value
+if show_true:
+    dta = df2
+else:
+    dta = df2[df2['bp_label'] == False]
 
 
-    # Create a selection for the tooltip
-    selection = alt.selection_single(fields=['timestamp'], nearest=True, on='mouseover', empty='none')
+# Create a selection for the tooltip
+selection = alt.selection_single(fields=['timestamp'], nearest=True, on='mouseover', empty='none')
 
-    # Create the chart
-    chart1 = alt.Chart(dta).mark_circle(size=100).encode(
-        x='timestamp:T',
-        y='sentiment:Q',
-        color=alt.Color('bp_label:N', scale=alt.Scale(domain=['True', 'False'], range=['red', 'blue'])),
-        tooltip=['tweet']
-    ).add_selection(selection)
+# Create the chart
+chart1 = alt.Chart(dta).mark_circle(size=100).encode(
+    x='timestamp:T',
+    y='bp_label:N',
+    color=alt.Color('bp_label:N', scale=alt.Scale(domain=['True', 'False'], range=['red', 'blue'])),
+    tooltip=['tweet']
+).add_selection(selection)
 
-    # Add a line that follows the selected timestamp
-    line1 = chart1.transform_filter(selection).mark_line(color='black')
+# Add a line that follows the selected timestamp
+line1 = chart1.transform_filter(selection).mark_line(color='black')
 
-    # Add the chart and line to the Streamlit app
-    st.altair_chart(chart1+line1, use_container_width=True)
+# Add the chart and line to the Streamlit app
+st.altair_chart(chart1+line1, use_container_width=True)
