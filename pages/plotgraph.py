6fc492acf090e2f3b192e401aa1520ea022f1262
Jude Sajith <65233121+SajithJude@users.noreply.github.com>
2023-03-01 00:13:15 +0000

    mmm

--- pages/plotgraph.py
+++ pages/plotgraph.py
@@ -40,4 +40,4 @@ if uf is not None:
     line1 = chart1.transform_filter(selection).mark_line(color='black')
 
     # Add the chart and line to the Streamlit app
-    st.altair_chart((chart1,line1).interactive(), use_container_width=True)
+    st.altair_chart((line1).interactive(), use_container_width=True)
